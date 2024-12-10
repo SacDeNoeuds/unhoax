c4fdb912a7338a8ef5e36c1c9d6d3d7f3f4e9c57
SacDeNoeuds <sacdenoeuds.dev@gmail.com>
2024-12-10 07:27:20 +0100

    use istanbul coverage reporter instead of v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     coverage: {
       ...(isCI && { reporter: ['json-summary'] }),
+      provider: 'istanbul',
       include: ['src'],
       exclude: ['./src/main.ts', '**/*.test.ts'],
     },
