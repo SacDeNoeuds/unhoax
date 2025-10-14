ae53723b28d2a36e22ac8b809868dfee7adab579
SacDeNoeuds <92589074+SacDeNoeuds@users.noreply.github.com>
2025-10-14 07:03:37 +0200

    feat: expose type-driven API under `typed<TheType>().object(schemas)`

---
+++ src/builder/typed.ts
@@ -0,0 +1,48 @@
+import {
+  object,
+  type ObjectSchema,
+  type ObjectShape,
+  type PropsOf,
+} from './object'
+
+export interface Typed<T> {
+  /**
+   * @category Reference
+   * @example
+   * ```ts
+   * interface Person {
+   *   name: string
+   * }
+   * const person = x.typed<Person>().object({ name: x.string })
+   * assert(person.parse({ name: 'Jack' }).success === true)
+   * assert(person.parse({ name: 42 }).success === false)
+   * assert(person.name === 'object')
+   * ```
+   */
+  object<Schemas extends PropsOf<Extract<T, ObjectShape>>>(
+    schemas: Schemas,
+  ): ObjectSchema<Extract<T, ObjectShape>, Schemas>
+  /**
+   * @category Reference
+   * @example
+   * ```ts
+   * interface Person {
+   *   name: string
+   * }
+   * const person = x.typed<Person>().object('Person', { name: x.string })
+   * assert(person.parse({ name: 'Jack' }).success === true)
+   * assert(person.parse({ name: 42 }).success === false)
+   * assert(person.name === 'Person')
+   * ```
+   */
+  object<Schemas extends PropsOf<Extract<T, ObjectShape>>>(
+    name: string,
+    schemas: Schemas,
+  ): ObjectSchema<Extract<T, ObjectShape>, Schemas>
+}
+
+export function typed<T>(): Typed<T> {
+  return {
+    object: object as unknown as Typed<T>['object'],
+  }
+}
